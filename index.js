6e652229916556a67e090e1c83fb67a4b2af34a1
Rob <robert.gentile@powerhrg.com>
2021-05-31 16:33:16 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const oldHeader = "h1"
+
+document.querySelector("#main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Rob is the champion!"
+newHeader.className = "victory"
